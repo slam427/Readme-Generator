3a36696d0e7716fbd03ceca97844675313936fa5
Steven Lam <stevenvl@gmail.com>
2020-02-28 14:57:35 -0500

    created questions and started the github api call

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,35 @@
+var inquirer = require("inquirer");
+var fs = require("fs");
+var api = require("utils/api.js");
+
 const questions = [
-
+    {name: "title",
+    message: "What is the title of your project?",
+    type: "input"
+    },
+    {
+    name: "userName",
+    message: "What is your GitHub username?",
+    type: "input"
+    },
+    {
+    name: "repoLocation",
+    message: "What is the location of your respository?",
+    type:"input"
+    }
 ];
+inquirer.prompt(questions)
+.then(function(answers) {
+console.log(answers);
+api.getUser(answers.userName).then(function(data){
+data.repos_url
+}).catch(function(err){
+console.log(err);
+})
+});
 
+
+var fileName = "generatedReadME.md"
 function writeToFile(fileName, data) {
 }
 
--- Develop/utils/api.js
+++ Develop/utils/api.js
@@ -1,7 +1,12 @@
+var axios = require("axios");
 const api = {
   getUser(username) {
-
+  return axios.get(`https://api.github.com/users/${username}`)
+  .then(function(res) {
+  // console.log(res.data);
+  return res.data;
+  })
   }
-};
-
+}
+// api.getUser();
 module.exports = api;
